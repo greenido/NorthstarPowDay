ced11ed0f59eaff043639d63dbe677fe5381e47e
Ido Green <greenido@gmail.com>
2023-01-15 10:32:39 -0800

    Squashed commit of the following:
    commit 9afb6cd20459fac6b75c2438413537a24eb1c61b
    Author: Ido Green <greenido@gmail.com>
    Date:   Sun Jan 15 10:29:22 2023 -0800
    
        Update package.json
    
    commit ee8ebb6c7f44533767d05fe66168cefe990b26b7
    Author: Glitch (pow-day-northstar) <none>
    Date:   Sun Jan 15 18:25:48 2023 +0000
    
        🧉 fix the package.json dep
    
    commit 7e80ebdff1a36d3f90cded29425b3f18e2d0dca8
    Author: Glitch (pow-day-northstar) <none>
    Date:   Sun Jan 15 18:14:19 2023 +0000
    
        🎧 Updated with Glitch
    
    commit dbfa50b5772daa497092c71a1c00262ecbfe3c75
    Author: Glitch (pow-day-northstar) <none>
    Date:   Thu Dec 26 17:40:26 2019 +0000
    
        🐒 change the snow conditions to the middle (from the lower)

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-// 
+//
 // Is it a powder day at Northstar? :)
 //
 // @author: Ido Green | @greenido
 // @date: March 2018
-// @last update: Nov 2018
+// @last update: Dec 2019
 //
 // @see:
 // source for data: view-source:https://www.northstarcalifornia.com/the-mountain/mountain-conditions/snow-and-weather-report.aspx
@@ -11,78 +11,71 @@
 // https://github.com/greenido/bitcoin-info-action
 // http://expressjs.com/en/starter/static-files.html
 // http://www.datejs.com/
+
 //
 // init project pkgs
-const express = require('express');
-const ApiAiAssistant = require('actions-on-google').ApiAiAssistant;
-const bodyParser = require('body-parser');
-const request = require('request');
-const rp = require('request-promise')
+//
+const ApiAiAssistant = require("actions-on-google").DialogflowApp;
+const bodyParser = require("body-parser");
+const request = require("request");
+const rp = require("request-promise");
+const Map = require("es6-map");
+const dateJS = require("./dateLib.js");
+
+const express = require("express");
 const app = express();
-const Map = require('es6-map');
-const dateJS = require('./dateLib.js');
 
 // Pretty JSON output for logs
-const prettyjson = require('prettyjson');
-const toSentence = require('underscore.string/toSentence');
+const prettyjson = require("prettyjson");
+const toSentence = require("underscore.string/toSentence");
 
-app.use(bodyParser.json({type: 'application/json'}));
-app.use(express.static('public'));
+app.use(bodyParser.json({ type: "application/json" }));
+app.use(express.static("public"));
 
 //
 // http://expressjs.com/en/starter/basic-routing.html
 //
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+// Add a health check route in express
+app.get("/_health", (req, res) => {
+  res.status(200).send("ok 🥇");
 });
 
 //
 // proxy to our web UI
 //
 app.get("/snow/", function (request, response) {
-   //res.setHeader('Content-Type', 'application/json');
-  console.log('** /snow/ --> getSnowConditions **');
-  rp('https://www.onthesnow.com/california/northstar-california/skireport.html')
+  //res.setHeader('Content-Type', 'application/json');
+  console.log("** /snow/ --> getSnowConditions **");
+  rp("https://www.onthesnow.com/california/northstar-california/skireport.html")
     .then(function (body) {
-        // Process html
-        let html = body; 
-        let inx1 = html.indexOf('lastUpdated') + 11;
-        let inx2 = html.indexOf('>', inx1) + 1; // we got 2 > to skip
-        let inx3 = html.indexOf('<', inx2); 
-        let lastUpdate = html.substring(inx2, inx3).trim();
-        let lastUpdateStr = "";
-        if (lastUpdate != null && lastUpdate.length > 2) {
-          lastUpdateStr = "<br><small>( Updated: " + lastUpdate + " )</small>";
-        }
-        console.log("== lastUpdate: " + lastUpdate);
+      // Process html
+      let html = body;
+      let inx1 = html.indexOf("Last Updated:") + 11;
+      let inx2 = html.indexOf(">", inx1) + 10; // we got 2 > to skip
+      let inx3 = html.indexOf("</time", inx2);
+      let lastUpdate = html.substring(inx2, inx3).trim();
+      let lastUpdateStr = "";
+      if (lastUpdate != null && lastUpdate.length > 2) {
+        lastUpdateStr = "<br><small>🏄🏽‍♀️ " + lastUpdate + " 🏂</small>";
+      }
+      //console.log(`inx1: ${inx1} inx2: ${inx2} inx3: ${inx3}`);
+      console.log("== lastUpdate: " + lastUpdate);
+      let res = "";
+      res += lastUpdateStr;
+      inx1 = html.indexOf("styles_snow__3fbcm") + 20;
+      inx2 = inx1 + 6;
+      for (let i = 0; i < 18; i++) {
+        inx2 = html.indexOf("</div>", inx2 + 5);
+      }
 
-        inx3 = html.indexOf('Today<br>', inx3) + 9;
-        let inx4 = html.indexOf('bluePill', inx3) + 10;
-        let inx44 = html.indexOf('"<', inx4);
-        let snowToday = html.substring(inx4, inx44).trim();
-
-        // upper mountain conditions
-        let inx5 = html.indexOf('<p>Upper:</p>');
-        let inx6 = html.indexOf('bluePill', inx5) + 10;
-        let inx7 = html.indexOf('"<', inx6);
-        let upperSnow = html.substring(inx6, inx7).trim();
-
-        // lower mountain conditions
-        let inx5l = html.indexOf('<p>Lower:</p>');
-        let inx6l = html.indexOf('bluePill', inx5l) + 10;
-        let inx7l = html.indexOf('"<', inx6l);
-        let lowerSnow = html.substring(inx6l, inx7l).trim();
-
-        console.log("🏔 lastUpdate: " + lastUpdate + " | snowToday: " + snowToday + " upperSnow: " + upperSnow +" lowerSnow: " + lowerSnow);
-
-        if (snowToday == null || snowToday.length < 1) {
-          console.log("Could not find if there is powder today");
-          response.send("Could not find if there is powder today");
-        }
-
-        let res = "❄️ Today at Northstar we got " + snowToday + " inch of snow ❄️<br>In the upper 🏔 " +
-          upperSnow + " inch.<br>In the lower ⛰ " + lowerSnow + " inch. " + lastUpdateStr;
-        response.send(res);
+      let allDataDiv = html.substring(inx1, inx2);
+      //console.log(`== inx1: ${inx1} inx2: ${inx2}  dataDiv: ${allDataDiv}`);
+      res += allDataDiv;
+      response.send(res);
     })
     .catch(function (err) {
       var errStr = "Error occurred. Err: " + JSON.stringify(err);
@@ -90,26 +83,19 @@ app.get("/snow/", function (request, response) {
     });
 });
 
-// Calling GA to make sure how many invocations we had on this skill
-const GAurl = "https://ga-beacon.appspot.com/UA-65622529-1/pow-day-northstar-server/?pixel=0";
-request.get(GAurl, (error, response, body) => {
-  console.log(" - Called the GA - " + new Date());
-});
-
-
 //
 // Handle webhook requests
 //
-app.post('/', function(req, res, next) {
+app.post("/", function (req, res, next) {
   //logObject("-- req: " , req);
   //logObject("-- res: " , res);
-  
+
   // Instantiate a new API.AI assistant object.
-  const assistant = new ApiAiAssistant({request: req, response: res});
+  const assistant = new ApiAiAssistant({ request: req, response: res });
   //let flightDate = assistant.getArgument('date');
   // Declare constants for your action and parameter names
-  const KEYWORD_ACTION = 'is-powder-day'; 
-  
+  const KEYWORD_ACTION = "is-powder-day";
+
   //
   // trim words so we won't talk for more than 2 minutes.
   //
@@ -117,16 +103,16 @@ app.post('/', function(req, res, next) {
     if (text == null) {
       return "";
     }
-    
+
     var words = text.match(/\S+/g).length;
     var trimmed = text;
     if (words > limit) {
-        // Split the string on first X words and rejoin on spaces
-        trimmed = text.split(/\s+/, limit).join(" ");
+      // Split the string on first X words and rejoin on spaces
+      trimmed = text.split(/\s+/, limit).join(" ");
     }
     return trimmed;
   }
-  
+
   //
   // Clean the text we are getting from the API so it will work great with voice only
   //
@@ -138,92 +124,117 @@ app.post('/', function(req, res, next) {
     cleanStr = cleanStr.replace(/\\n/g, "");
     return cleanStr;
   }
-  
+
   //
   // Coz wikipedia api return some data fields not inside tags :/
   //
   function cleanHTMLTags(html) {
     if (html != null && html.length > 1) {
-      let text = html.replace(/<(?:.|\n)*?>/gm, '');
+      let text = html.replace(/<(?:.|\n)*?>/gm, "");
       let inx1 = 0;
       let foundDataField = text.indexOf("data-");
       while (inx1 < text.length && foundDataField > 0) {
         let inx2 = text.indexOf(">", inx1) + 1;
         if (inx2 < inx1) {
-          inx2 = text.indexOf("\"", inx1) + 1;
-          inx2 = text.indexOf("\"", inx2) + 2;
+          inx2 = text.indexOf('"', inx1) + 1;
+          inx2 = text.indexOf('"', inx2) + 2;
         }
-        text = text.substring(0,inx1) + text.substring(inx2, text.length);
+        text = text.substring(0, inx1) + text.substring(inx2, text.length);
         inx1 = inx2 + 1;
         foundDataField = text.indexOf("data-", inx1);
-      } 
-      return text;  
+      }
+      return text;
     }
     //
     return html;
   }
-  
+
   //
   // Create functions to handle intents here
+  // TODO: fix it :)
   //
   function getSnowInfo(assistant) {
-    console.log('** Handling action: ' + KEYWORD_ACTION );
-    request({ method: 'GET',
-             url:'https://www.onthesnow.com/california/northstar-california/skireport.html'},
-            function (err, response, body) {
+    console.log("** Handling action: " + KEYWORD_ACTION);
+    request(
+      {
+        method: "GET",
+        url: "https://www.onthesnow.com/california/northstar-california/skireport.html",
+      },
+      function (err, response, body) {
         if (err) {
-            console.log("An error occurred. Err: " + JSON.stringify(err));
-            assistant.tell("Sorry something is not working at the moment. Please try again later and be happy.");
-            return;
+          console.log("An error occurred. Err: " + JSON.stringify(err));
+          assistant.tell(
+            "Sorry something is not working at the moment. Please try again later and be happy."
+          );
+          return;
         }
-        try {  
-          let html = response.body; 
-          let inx1 = html.indexOf('lastUpdated') + 11;
-          let inx2 = html.indexOf('>', inx1) + 1; // we got 2 > to skip
-          let inx3 = html.indexOf('<', inx2); 
+        try {
+          let html = response.body;
+          let inx1 = html.indexOf("lastUpdated") + 11;
+          let inx2 = html.indexOf(">", inx1) + 1; // we got 2 > to skip
+          let inx3 = html.indexOf("<", inx2);
           let lastUpdate = html.substring(inx2, inx3).trim();
           //console.log("== lastUpdate: " + lastUpdate);
-          
-          inx3 = html.indexOf('Today<br>', inx3) + 9;
-          let inx4 = html.indexOf('bluePill', inx3) + 10;
+
+          inx3 = html.indexOf("Today<br>", inx3) + 9;
+          let inx4 = html.indexOf("bluePill", inx3) + 10;
           let inx44 = html.indexOf('"<', inx4);
           let snowToday = html.substring(inx4, inx44).trim();
-          
+
           // upper mountain conditions
-          let inx5 = html.indexOf('<p>Upper:</p>');
-          let inx6 = html.indexOf('bluePill', inx5) + 10;
+          let inx5 = html.indexOf("<p>Upper:</p>");
+          let inx6 = html.indexOf("bluePill", inx5) + 10;
           let inx7 = html.indexOf('"<', inx6);
           let upperSnow = html.substring(inx6, inx7).trim();
-           
+
           // lower mountain conditions
-          let inx5l = html.indexOf('<p>Lower:</p>');
-          let inx6l = html.indexOf('bluePill', inx5l) + 10;
+          let inx5l = html.indexOf("<p>Lower:</p>");
+          let inx6l = html.indexOf("bluePill", inx5l) + 10;
           let inx7l = html.indexOf('"<', inx6l);
           let lowerSnow = html.substring(inx6l, inx7l).trim();
-          
-          console.log("== lastUpdate: " + lastUpdate + " | snowToday: " + snowToday + " upperSnow: " + upperSnow +" lowerSnow: " + lowerSnow);
-  
+
+          console.log(
+            "== lastUpdate: " +
+              lastUpdate +
+              " | snowToday: " +
+              snowToday +
+              " upperSnow: " +
+              upperSnow +
+              " lowerSnow: " +
+              lowerSnow
+          );
+
           if (snowToday == null || snowToday.length < 1) {
-            assistant.ask("Could not find if there is powder today. See you later!");
+            assistant.ask(
+              "Could not find if there is powder today. See you later!"
+            );
             return;
           }
-          
-          let res = "Today at Northstar we got " + snowToday + " inch of snow. In the upper mountain we have " +
-            upperSnow + " inch and in the lower moutain there are " + lowerSnow + " inch. Have an amazing day and be safe!"; 
+
+          let res =
+            "Today at Northstar we got " +
+            snowToday +
+            " inch of snow. In the upper mountain we have " +
+            upperSnow +
+            " inch and in the lower moutain there are " +
+            lowerSnow +
+            " inch. Have an amazing day and be safe!";
           assistant.tell(res);
+        } catch (error) {
+          console.log(
+            "(!) Error: " + error + " json: " + JSON.stringify(error)
+          );
         }
-        catch(error) {
-          console.log("(!) Error: " + error + " json: "+ JSON.stringify(error));
-        }
-    }); //
+      }
+    ); //
   }
-  
+
   //
   // Add handler functions to the action router.
   //
   let actionRouter = new Map();
   actionRouter.set(KEYWORD_ACTION, getSnowInfo);
-  
+
   // Route requests to the proper handler functions via the action router.
   assistant.handleRequest(actionRouter);
 });
@@ -233,8 +244,10 @@ app.post('/', function(req, res, next) {
 //
 app.use(function (err, req, res, next) {
   console.error(err.stack);
-  res.status(500).send('Oppss... could not check when is the next flight to space.');
-})
+  res
+    .status(500)
+    .send("Oppss... could not check when is the next flight to space.");
+});
 
 //
 // Pretty print objects for logging
@@ -248,5 +261,7 @@ function logObject(message, object, options) {
 // Listen for requests -- Start the party
 //
 let server = app.listen(process.env.PORT, function () {
-  console.log('--> Our Webhook is listening on ' + JSON.stringify(server.address()));
+  console.log(
+    "--> Our Webhook is listening on " + JSON.stringify(server.address())
+  );
 });
